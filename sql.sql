c32661e69a629862aa508e9675c8755191dacd4e
joogka <jhs214949@gmail.com>
2023-11-17 19:25:56 +0900

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -8,7 +8,14 @@ create table member(
     name varchar(10) not null,
     birth varchar(10) not null
 );
+
+select * from member;
+
+insert into member values('qwe','qwe','qwe@gmail.com','정명진','2002-07-06');
 drop table member;
+drop table friend_add;
+drop table writing;
+drop table comment;
 	create table writing(
 		id varchar(30) not null,
 		num int auto_increment primary key, -- 글번호
@@ -16,20 +23,23 @@ drop table member;
 		date datetime default now() not null, -- 등록날짜
 		looknum int not null	              -- 조회수
 		);
+        insert into writing values();
+select * from member;
+select * from friend_add;
+drop table likes;
 drop table writing;
 create table likes( -- 좋아요
 	me_id varchar(20) not null,
     likes tinyint(0) not null,
-    wr_num int not null,
-    constraint fk_wr_num foreign key(wr_num) REFERENCES writing(num),
-    constraint fk_me_id foreign key(me_id) REFERENCES member(id)
+    wr_num int not null
 );
 
 create table friend_add(
-	friend_id varchar(10),
-    constraint fk_fr_id foreign key(friend_id) references member(id)
+	member_id int not null, -- 현재 로그인중인 id
+	friend_id varchar(10) -- 현재 로그인중인 id가 추가한 친구id
 );
-
+select * from friend_add where member_id = 1;
+drop table friend_add;
 create table profile(
     my_profile varchar(50) -- 내소개
 );
@@ -43,3 +53,4 @@ create table comment(
 
 desc likes;
 drop table likes;
+commit;
